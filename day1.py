0a90a1a9e971b5eedd0a6f9c6538637e9853deef
shreyasicodes20 <97606334+shreyasicodes20@users.noreply.github.com>
2022-02-11 21:56:36 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,19 @@
+#Datatypes and Type Conversions
+ 
+name='shreyasi' #string Datatype
+Age=20 #integer Datatype
+height=173.5 #Float Datatype
+print(type(Age))'''
+ 
+number1=3
+number2=5.6
+output=number1+number2
+print(output)
+
+'''Arithmetic operators
+Assignment Operator
+Comparison Operator
+Logical Operator
+Identity Operator
+Membership Operators
+'''
